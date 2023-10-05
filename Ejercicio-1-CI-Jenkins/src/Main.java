7ee678d055ced3abd3eb4de9d2c16a85f4ca7056
renemorales <renemoralesalejandro@gmail.com>
2023-10-04 22:32:39 -0300

    Segunda commit

--- Ejercicio-1-CI-Jenkins/src/Main.java
+++ Ejercicio-1-CI-Jenkins/src/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("A commit has been made.");
+        System.out.println("A second commit has been made.");
     }
     
 }
